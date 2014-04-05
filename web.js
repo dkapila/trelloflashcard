e4e6854a942c8a12915e236ecd45406ab4c4ab46
Dharam Kapila <dharamkapila@gmail.com>
2014-04-05 12:58:00 +0200

    Adding routelist file

--- web.js
+++ web.js
@@ -1,13 +1,28 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var routes = require('./routes/routelist')
+var path = require('path');
 var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get('/', routes.index);
+
+
+
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
+
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
